3bee452c39015d9a16179e3673d7194a0eae7ab8
nicolechecker <nicolechecker@icloud.com>
2025-08-05 22:34:29 -0600

    Exec page

--- laicsite/src/App.jsx
+++ laicsite/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HeroHome from './pages/HeroHome';
 import Events from './pages/Events';
+import Exec from './pages/Exec';
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HeroHome />} />
         <Route path="/events" element={<Events />} />
+        <Route path="/exec" element={<Exec/>}/>
       </Routes>
     </BrowserRouter>
   );
